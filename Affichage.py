9d578874ec6d5e766105fe9e33ee17cbcfd36534
Gouderg <victor.illien974@gmail.com>
2022-10-03 20:42:45 +0200

    Temp

--- Affichage.py
+++ Affichage.py
@@ -1,6 +1,36 @@
 import tkinter as tk
+from constante import LARGEUR, HAUTEUR
+import time
 
-class Affichage:
+# https://stackoverflow.com/questions/29158220/tkinter-understanding-mainloop
+class Affichage(tk.Tk):
 
     def __init__(self):
+        tk.Tk.__init__(self)
+
+        self.canvas = tk.Canvas(self, bg='white', width=LARGEUR, height=HAUTEUR)
+        self.canvas.pack()
+
+        # Create points.
+        self.create_nodes()
+
+        # Bind keys.
+        self.canvas.bind("<Escape>", self.stop)
+    
+    def update(self, i, j):
+        self.canvas.create_rectangle(30, 70, i, j)
+
+    
+    def create_nodes(self):
         pass
+
+    def stop(self):
+        self.quit()
+
+app = Affichage()
+
+for i in range(1000):
+
+    app.update(i, i)
+    time.sleep(0.05)
+    print(1)
