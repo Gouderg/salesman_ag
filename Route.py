6ac9ca8c74b3efd4f0c2b8c25d9bd8c63ff46c5d
Gouderg <victor.illien974@gmail.com>
2022-10-07 10:18:54 +0200

    Fix error

--- Route.py
+++ Route.py
@@ -2,14 +2,13 @@ class Route:
 
     def __init__(self):
         self.ordre = []
-        self.fitness = 0
 
     def __getitem__(self, index):
         return self.ordre[index]
 
     def __repr__(self) -> str:
-        a = [str(i) for i in self.ordre]
-        return 'Ordre: '+', '.join(a)
+        a = [str(elt) for elt in self.ordre]
+        return '"Ordre: '+', '.join(a) + '"'
     
     def __len__(self):
         return len(self.ordre)
