6dfb92695b86c1aaa82ddc654e307a96d0f15a6c
Gouderg <victor.illien974@gmail.com>
2022-10-07 10:35:33 +0200

    Fitness

--- TSP_GA.py
+++ TSP_GA.py
@@ -15,8 +15,8 @@ class TSP_GA:
                 a = random.choice(villes)
                 route.addValue(a)
                 villes.remove(a)
+            route.addValue(route[0])
             self.routes.append(route)
-        print(self.routes)
 
     def calculate_fitness(self, graph, index):
         return 1/graph.calcul_distance_route(self.routes[index])
@@ -29,5 +29,18 @@ if __name__ == "__main__":
     g = Graph()
 
     algo = TSP_GA()
+
+    # On génère une population de route.
     algo.generate_routes(g)
-    print(algo.calculate_fitness(g, 0))
+    
+    # On itère un certain nombre de fois
+    for ite in range(NB_ITERATION):
+
+        # Calcul du fitness de chaque Route et on normalise.
+        algo.fitness = [algo.calculate_fitness(g, f) for f in range(NB_LIEUX)]
+        algo.fitness = [algo.fitness[i]/sum(algo.fitness) for i in range(NB_LIEUX)]
+
+        print(algo.fitness)
+
+        break
+    print()
