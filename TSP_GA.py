c44043d5dc5806c9b06bff1a6158669289b762a2
Louis Froehlicher <louis.froehlicher.lof@gmail.com>
2022-10-07 09:32:36 +0200

    Graph and Lieu changes Graph :Function to generate a table of points Lieu :Function to calculate euclidian distance

--- TSP_GA.py
+++ TSP_GA.py
@@ -1,11 +1,33 @@
-import tsp_graph_init
+from tsp_graph_init import *
+
 
 class TSP_GA:
+    def __init__(self):
+        self.routes = []
 
-    def main():
+
+    def generate_routes(self, graph):
+        for j in range(NB_POPULATION):
+            villes = list(graph.list_lieu)
+            route = Route()
+            for i in range(NB_LIEUX):
+                a = random.choice(villes)
+                route.addValue(a)
+                villes.remove(a)
+            self.routes.append(route)
+        print(self.routes)
+
+    def calculate_fitness(self, index, graph):
+
+        return 1/graph.calcul_distance_route(self.routes[index])
+
+    def main(self):
         print("Launch application.")
 
 
 if __name__ == "__main__":
+    g = Graph()
 
-    TSP_GA.main()
+    algo = TSP_GA()
+    algo.generate_routes(g)
+    print(algo.calculate_fitness(0, g))
