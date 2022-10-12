a353f0af72aadba4e9e3cceb40860aa3fa6f29c1
Louis Froehlicher <louis.froehlicher.lof@gmail.com>
2022-10-12 16:52:08 +0200

    Better Affichage Test main

--- Affichage.py
+++ Affichage.py
@@ -10,13 +10,17 @@ class Affichage(tk.Tk):
 
         self.title("Salesman Gen")
         self.canvas = tk.Canvas(self, bg='white', width=LARGEUR, height=HAUTEUR)
+
         self.canvas.pack()
 
+        self.present_paths = []
 
         self.infoText = tk.StringVar()
 
+
         self.text1 = tk.Label(self, textvariable=self.infoText)
-        self.text1.pack()
+
+        self.text1.pack(side=tk.BOTTOM)
 
         # Create points.
         self.create_nodes(list_lieux)
@@ -25,25 +29,34 @@ class Affichage(tk.Tk):
         self.bind("<Escape>", self.stop)
     
     def draw(self, nb_iteration, best_path):
-        self.infoText.set("Nombre itérations: " +str(nb_iteration)+", Meilleur chemin: "+' - '.join(best_path))
+        #self.infoText.set("Nombre itérations: " +str(nb_iteration))
+        #self.infoText.set("Nombre itérations: " +str(nb_iteration)+", Meilleur chemin: "+best_path)
 
-    def draw_path(self,node1,node2):
-        self.canvas.create_line(node1.x, node1.y, node2.x, node2.y, arrow=tk.LAST)
+        self.infoText.set("Nombre itérations: " + str(nb_iteration)+", Meilleur chemin: "+ str(best_path[1]))
 
+    def draw_path(self, node1, node2):
+        self.present_paths.append(self.canvas.create_line(node1.x, node1.y, node2.x, node2.y, arrow=tk.LAST, width=1))
 
+    def draw_path_color(self, node1, node2):
+        self.present_paths.append(self.canvas.create_line(node1.x, node1.y, node2.x, node2.y, arrow=tk.LAST, fill='red', width=1, offset=20))
 
-    
+    def reset_paths(self):
+        for i in range(len(self.present_paths)):
+            self.canvas.delete(self.present_paths[i])
+
+        self.present_paths = []
+
     def create_nodes(self, list_lieux):
+
         #self.canvas.create_oval(0, 0, 15, 15)
         #self.canvas.create_oval(LARGEUR-15, HAUTEUR-15, LARGEUR, HAUTEUR)
-
-
 
         for lieu in list_lieux:
             x, y = lieu.x, lieu.y
 
             self.canvas.create_oval(x-15, y-15, x+15, y+15)
-        
+
+
 
     def stop(self, event):
         # self.withdraw()
--- main2.py
+++ main2.py
@@ -1,22 +1,59 @@
 from tsp_graph_init import *
+from tkinter import *
 import time
 import numpy as np
 from TSP_GA import TSP_GA
 
+def takeDistance(element):
+    return element[0]
+
 def main():
-    g = Graph()
+    best_routes = []
+
+    graph = Graph()
     algo = TSP_GA()
-    algo.generate_routes(g)
+    algo.generation_population(graph)
 
-    route_to_expose = algo.routes[0]
 
-    app = Affichage(g.list_lieu)
+    app = Affichage(graph.list_lieu)
+
     for i in range(1000):
+
+        # Reproduction.
+        if algo.reproduction(graph) == -1: break
+
+        # Prération pour la prochaine itération.
+        algo.reset()
+
+        # Meilleur coup de l'itération.
+        d, r = algo.find_best_move(graph)
+
+        if len(best_routes) == 0:
+            best_routes.append((d, r))
+        else:
+            if d < best_routes[0][0]:
+                best_routes.append((d, r))
+                best_routes.sort(reverse=True, key=takeDistance)
+                if len(best_routes) > 10:
+                    best_routes.pop(0)
+
+        # Update de l'affichage graphique.
         for j in range(9):
-            app.draw_path(g.list_lieu[route_to_expose[j]], g.list_lieu[route_to_expose[j+1]])
+            app.draw_path(graph.list_lieu[r[j]], graph.list_lieu[r[j+1]])
+
+        for k in range(len(best_routes)):
+            for l in range(9):
+                app.draw_path_color(graph.list_lieu[best_routes[k][1][l]], graph.list_lieu[best_routes[k][1][l+1]])
+
+
+        #print(i, d, r)
+        app.draw(i, best_routes[len(best_routes)-1])
+        #app.draw(str(i), r.ordre)
+
         app.update()
         app.update_idletasks()
-        time.sleep(0.02)
+
+        app.reset_paths()
 
 
 if __name__ == "__main__":
